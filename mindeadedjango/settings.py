fc1653c2d5eaa389455a936745435790314acee2
biplab ganguly <biplab123.abcd@gamil.com>
2024-02-17 23:53:26 +0530

    allow all hosts

--- mindeadedjango/settings.py
+++ mindeadedjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-om1qw2vi7)blqo&y-)j#0(vl0cyrx2zxqp4x!j(qjavlm*^*k=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
